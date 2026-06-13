f1fbaecbe6348ba1b2136f0ce7cc784f63af987c
Viktor Nikolov <viktor.nikolov@students.finki.ukim.mk>
2026-06-13 04:05:11 +0200

    Implement book search

--- SI2026Lab1Main.java
+++ SI2026Lab1Main.java
@@ -53,6 +53,11 @@ class Library {
 
     // TODO: Implement in branch feature-search-books
     public boolean searchBookByTitle(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -120,5 +125,7 @@ public class SI2026Lab1Main {
         library.addBook(new Book("1984", "George Orwell", "Dystopian"));
 
         System.out.println("Library initialized.");
+
+        System.out.println("Search '1984': " + library.searchBookByTitle("1984"));
     }
 }

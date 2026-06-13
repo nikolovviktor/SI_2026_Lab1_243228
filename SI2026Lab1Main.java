1cac5971b40afea21531689885e6e93bbdcaf38f
Viktor Nikolov <viktor.nikolov@students.finki.ukim.mk>
2026-06-13 04:25:52 +0200

    Implement borrow book

--- SI2026Lab1Main.java
+++ SI2026Lab1Main.java
@@ -67,7 +67,7 @@ class Library {
             if (book.getTitle().equalsIgnoreCase(title)) {
                 if (!book.isBorrowed()) {
                     book.setBorrowed(true);
-                    System.out.println("Book borrowed.");
+                    System.out.println("Borrowed successfully.");
                 } else {
                     System.out.println("Book is already borrowed.");
                 }
@@ -123,6 +123,7 @@ public class SI2026Lab1Main {
         library.addBook(new Book("Effective Java", "Joshua Bloch", "Programming"));
         library.addBook(new Book("The Hobbit", "J.R.R. Tolkien", "Fantasy"));
         library.addBook(new Book("1984", "George Orwell", "Dystopian"));
+	library.borrowBook("The Hobbit");
 
         System.out.println("Library initialized.");
 
